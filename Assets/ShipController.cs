4a7738c3806a76762cee63e5135c27b2d9807b57
Dennis McWilliams <d12768@gmail.com>
2018-09-08 05:51:53 -0400

    1

---
+++ Assets/ShipController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 move = Vector3.zero;
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+        transform.position += move;
+    }
+}
